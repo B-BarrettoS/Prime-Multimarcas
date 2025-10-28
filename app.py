3c360b1f0de744c75f6416ec4e43a6b152670b67
Bruno Barreto <brunobcsales@icloud.com>
2025-10-28 11:31:49 -0300

    Adiciona página de joias

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/joias')
+def joias():
+    return render_template('joias.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
